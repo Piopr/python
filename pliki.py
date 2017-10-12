ab41c900d276094c796462b7087a37eb5411b63a
Piopr <32546106+Piopr@users.noreply.github.com>
2017-10-12 11:43:34 +0200

    Add files via upload

---
+++ pliki.py
@@ -0,0 +1,10 @@
+tydzien=["poniedzialek","wtorek","sroda","czwartek","piatek","sobota","niedziela"]
+plik = open("pliczek.txt", "w")
+plik.write("siemano")
+for x in tydzien:
+    plik.write(x)
+plik.close
+#plik2 = open("pliczek2.txt", "r")
+#print file.read();
+             
+

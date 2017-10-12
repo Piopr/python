2b8afe2a28b00b7f37d3a224378bb4fdc7370e7d
Piopr <32546106+Piopr@users.noreply.github.com>
2017-10-12 11:01:00 +0200

    Add files via upload

--- siema1.py
+++ siema1.py
@@ -1,1 +1,15 @@
 print("siema")
+x=7
+y="Marek"
+z=7/2 #dzielenie zmiennoprzecinkowe
+z1=7//2 #dzielenie bez przecinka
+z2= 2**9 #potega
+st="zenek"
+st1=""" siema siema
+witam witam
+co tam co tam """
+st2="Pozdrowienia"+"do wiezienia"
+st3="Rozmiar buta "+str(45)
+print(st3)
+lista=["a","b","c"]
+print(lista[2])

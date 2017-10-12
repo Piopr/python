a2d4e6806e389cb979735a547e2b61646e5f015c
Piopr <32546106+Piopr@users.noreply.github.com>
2017-10-12 11:23:38 +0200

    Add files via upload

---
+++ dnitygodniav2.py
@@ -0,0 +1,6 @@
+tydzien=["poniedzialek","wtorek","sroda","czwartek","piatek","sobota","niedziela"]
+for x in tydzien:
+    if len(x)%2==0:
+        for y in range(len(x), -1, -1):
+                                print(y)
+    
